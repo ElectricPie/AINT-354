c3ca94dbe3aada7baaa4f569ce9788010f2fe41b
Connor Priest <32139670+ElectricPie@users.noreply.github.com>
2018-10-19 11:50:28 +0100

    Created Base Editor Window
    Added RPG Tool window to the unity widowns dropdown.

---
+++ Assets/Editor/RPGTool.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RPGTool : EditorWindow {
+    [MenuItem("Window/RPG Tool")]
+
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(RPGTool));
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+}
