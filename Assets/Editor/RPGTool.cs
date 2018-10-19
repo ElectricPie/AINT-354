b35e3c685f49c3e93b106e4c66a3f7ad621567f3
Connor Priest <32139670+ElectricPie@users.noreply.github.com>
2018-10-19 13:33:29 +0100

    Changing Tabs Changes Content
    Changing the tags now changes what GUI elements are rendered.

--- Assets/Editor/RPGTool.cs
+++ Assets/Editor/RPGTool.cs
@@ -24,21 +24,41 @@ public class RPGTool : EditorWindow {
 
     private void OnGUI()
     {
-        string test = "Hello World!";
+        
 
         //Creates and updates tabs
         currentTab = GUILayout.Toolbar(currentTab, new string[] { "Test 1", "Test 2", "Test 3"});
 
+        switch (currentTab)
+        {
+            case 0:
+                DisplayTabOne();
+                break;
+            case 1:
+                DisplayTabTwo();
+                break;
+        }        
+    }
+
+    //Tab 1
+    private void DisplayTabOne()
+    {
+        string test = "Hello World!";
+
         //Creates label
         GUILayout.Label("Test GUI", EditorStyles.boldLabel);
         //Creates editable text field
-        EditorGUILayout.TextField("Text Field", test);
+        test = EditorGUILayout.TextField("Text Field", test);
+    }
 
+    //Tab 2
+    private void DisplayTabTwo()
+    {
         //Creates lable
         GUILayout.Label("Characters", EditorStyles.boldLabel);
-        
+
         //Creates and checks if button is pressed
-        if(GUILayout.Button("Create Character"))
+        if (GUILayout.Button("Create Character"))
         {
             //Debug messages to show if button is pressed
             Debug.Log("Editor Button Pressed");
