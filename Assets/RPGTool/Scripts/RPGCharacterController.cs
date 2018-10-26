9734d14161889a344c0e82481d0ccc580ccdb895
ElectricPie <32139670+ElectricPie@users.noreply.github.com>
2018-10-26 21:18:44 +0100

    Created Basic RPG Character
    Create a scene for use in testing using prototype materials fromhttps://assetstore.unity.com/packages/2d/textures-materials/abstract/early-prototyping-material-kit-51761.
    Created basic RPG character controls using horizontal and vertical inputs.

---
+++ Assets/RPGTool/Scripts/RPGCharacterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPGCharacterController : MonoBehaviour {
+
+    public float speed = 10;
+
+    private Rigidbody m_rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        m_rigidbody = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+	}
+}
