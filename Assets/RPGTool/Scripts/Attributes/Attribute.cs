5f0fdf70b4a6f132b9162c51d646c56901a31fd8
Connor Priest <32139670+ElectricPie@users.noreply.github.com>
2018-11-01 11:22:23 +0000

    Added Attibute Base
    Added the base for attributes.
    RPGCharacterController can have attributes added.
    Attributes store their name, short variants of the name, description, their base value and current value

---
+++ Assets/RPGTool/Scripts/Attributes/Attribute.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attribute  {
+    private string m_name;
+    private string m_sName;
+    private string m_disc;
+    private int m_baseValue;
+    private int m_value;
+
+    public Attribute(string name, string sName, string disc, int baseValue)
+    {
+        m_name = name;
+        m_sName = sName;
+        m_disc = disc;
+        m_baseValue = baseValue;
+        m_value = m_baseValue;
+    }
+
+    public string Name
+    {
+        get { return m_name; }
+
+        set { m_name = value; }
+    }
+
+    public string ShortName
+    {
+        get { return m_sName; }
+
+        set { m_sName = value; }
+    }
+
+    public string Disc
+    {
+        get { return m_disc; }
+
+        set { m_disc = value; }
+    }
+
+    public int BaseValue
+    {
+        get { return m_baseValue; }
+
+        set { m_baseValue = value; }
+    }
+}
