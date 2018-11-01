c0a433d80da9fa413116371459e2ab308ae914e6
ElectricPie <32139670+ElectricPie@users.noreply.github.com>
2018-11-01 18:39:05 +0000

    Implemented Attribute Editing
    Once at least one attribute has been added a new edit section will be drawn under the scroll view.
    Changing the fields and pressing the edit attribute button will change the selected attributes values.

--- Assets/Editor/RPGTool/Tabs/AttributesTab.cs
+++ Assets/Editor/RPGTool/Tabs/AttributesTab.cs
@@ -14,12 +14,20 @@ public class AttributesTab : Tab
     private string m_newAttributeDisc;
     private int m_newAttributeBaseValue;
 
+    private string m_editAttributeName;
+    private string m_editAttributeSName;
+    private string m_editAttributeDisc;
+    private int m_editAttributeBaseValue;
+
     private int m_tabState = 0;
+
+    private int m_attributeToEdit;
 
     public AttributesTab()
     {
         m_tabName = "Attributes";
         m_attributes = new List<Attribute>();
+        m_attributeToEdit = 0;
     }
 
     public override void DisplayTab()
@@ -27,33 +35,40 @@ public class AttributesTab : Tab
         switch (m_tabState)
         {
             case 0:
-
-
                 GUILayout.Label("Attributes", EditorStyles.boldLabel);
 
                 //Draws the scroll view
                 m_scrollPosition = GUILayout.BeginScrollView(m_scrollPosition, GUILayout.Width(200), GUILayout.Height(100));
 
+                //Draws the new attribute button
                 if (GUILayout.Button("New Attribute"))
                 {
                     m_tabState = 1;
                 }
 
+                //Draws all attributes in the scroll view
                 for (int i = 0; i < m_attributes.Count; i++)
                 {
-                    Debug.Log(m_attributes[i].Name);
-                    GUILayout.Label(m_attributes[i].Name);
+                    if (GUILayout.Button(m_attributes[i].Name))
+                    {
+                        m_attributeToEdit = i;
+                        UpdateEditDisplay();
+                    }
                 }
 
                 //Ends the scroll view
                 GUILayout.EndScrollView();
+
+                //Starts displaying the edit attribute section
+                if (m_attributes.Count > 0)
+                {
+                    EditAttribute();
+                }
                 break;
             case 1:
                 NewAttribute();
                 break;
         }
-
-        
     }
 
     //Tab state 1
@@ -81,12 +96,56 @@ public class AttributesTab : Tab
             //Adds the attribute to the list of attributes
             m_attributes.Add(newAttribute);
 
-            m_tabState = 0;
+            ResetNewAttributesTextField();
         }
 
         if (GUILayout.Button("Cancel"))
         {
-            m_tabState = 0;
+            ResetNewAttributesTextField();
+        }
+    }
+
+    private void ResetNewAttributesTextField()
+    {
+        //Sets the tabs state back to the scroll view
+        m_tabState = 0;
+
+        m_newAttributeName = "";
+        m_newAttributeSName = "";
+        m_newAttributeDisc = "";
+        m_newAttributeBaseValue = 0;
+
+        //Pulls foucs away allowing values to reset
+        GUI.FocusControl("");
+    }
+
+    private void UpdateEditDisplay()
+    {
+        //Updates the edit section with the needed attributes data
+        m_editAttributeName = m_attributes[m_attributeToEdit].Name;
+        m_editAttributeSName = m_attributes[m_attributeToEdit].ShortName;
+        m_editAttributeDisc = m_attributes[m_attributeToEdit].Disc;
+        m_editAttributeBaseValue = m_attributes[m_attributeToEdit].BaseValue;
+    }
+
+    private void EditAttribute()
+    {
+        GUILayout.Label("Edit Attribute", EditorStyles.boldLabel);
+
+        //Draws the field for getting edited attribute data
+        m_editAttributeName = EditorGUILayout.TextField("Name", m_editAttributeName);
+        m_editAttributeSName = EditorGUILayout.TextField("Short Name", m_newAttributeSName);
+        m_editAttributeDisc = EditorGUILayout.TextField("Discription", m_newAttributeDisc);
+        m_editAttributeBaseValue = EditorGUILayout.IntField("Base Value", m_newAttributeBaseValue);
+
+        if (GUILayout.Button("Edit Attribute"))
+        {
+            //Sets the new edited values
+            m_attributes[m_attributeToEdit].Name = m_editAttributeName;
+            m_attributes[m_attributeToEdit].ShortName = m_editAttributeSName;
+            m_attributes[m_attributeToEdit].Disc = m_editAttributeDisc;
+            m_attributes[m_attributeToEdit].BaseValue = m_editAttributeBaseValue;
+
         }
     }
 }
