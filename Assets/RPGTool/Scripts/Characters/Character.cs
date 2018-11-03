25a2feca854c107d9100114886b9802527722f96
ElectricPie <32139670+ElectricPie@users.noreply.github.com>
2018-11-03 21:28:41 +0000

    Created Character Base
    Created the base for characters which contains the characters health, resources and name.
    Also includes a method for the character to take damage and to check if the character is alive.

---
+++ Assets/RPGTool/Scripts/Characters/Character.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character {
+    private string m_name;
+    private int m_health;
+    private int m_resources;
+
+    private bool m_alive;
+
+
+    public void TakeDamage(int damage)
+    {
+        //Removes the damage from the characters health
+        m_health -= damage;
+
+        UpdateIfAlive();
+    }
+
+    private void UpdateIfAlive()
+    {
+        //Checks if the characters health is below the death threshold
+        if (m_health <= 0)
+        {
+            m_alive = false;
+        }
+        else
+        {
+            m_alive = true;
+        }
+    }
+
+    public string Name
+    {
+        get{ return m_name; }
+        set { m_name = value; }
+    }
+
+    public int Heath
+    {
+        get { return m_health; }
+    }
+
+    public int Resources
+    {
+        get { return m_resources; }
+    }
+
+    public bool Alive
+    {
+        get { return m_alive; }
+    }
+}
