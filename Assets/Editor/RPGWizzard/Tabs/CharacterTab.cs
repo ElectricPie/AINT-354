3b1988181d01ae6f8bdbe95104e5ca895341510e
Connor Priest <32139670+ElectricPie@users.noreply.github.com>
2018-11-20 15:58:16 +0000

    Added Area to Tab
    Added an area which adapts to the window changing shape.

--- Assets/Editor/RPGWizzard/Tabs/CharacterTab.cs
+++ Assets/Editor/RPGWizzard/Tabs/CharacterTab.cs
@@ -12,6 +12,28 @@ public class CharacterTab : Tab {
 
     public override void DisplayTab()
     {
-        
+        PlayerCharacterList();
+    }
+
+    private void PlayerCharacterList()
+    {
+        GUILayout.Label("Player Character", EditorStyles.boldLabel);
+
+        Debug.Log("Window Size: " + m_windowSize);
+
+        //Draws the main box and sets the width and height to adapt to the window changing size
+        GUILayout.BeginArea(new Rect(20, 50, m_windowSize.width - 40, m_windowSize.height - 70));
+
+        //Debug buttons to check area size
+        for (int i = 0; i < 100; i++)
+        {
+            if (GUILayout.Button("New Player"))
+            {
+
+            }
+        }
+
+        //Ends the main box
+        GUILayout.EndArea();
     }
 }
