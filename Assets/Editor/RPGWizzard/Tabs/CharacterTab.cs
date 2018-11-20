63d86159d29daa97d18e85cd221485f6eb4cc732
Connor Priest <32139670+ElectricPie@users.noreply.github.com>
2018-11-20 16:16:46 +0000

    Added Player Character Scroll View
    Added the character scroll view with testing buttons just under the width of the scroll view.
    Added variables to store the Rects for the main box and scroll view for use in calculations for other elements.

--- Assets/Editor/RPGWizzard/Tabs/CharacterTab.cs
+++ Assets/Editor/RPGWizzard/Tabs/CharacterTab.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class CharacterTab : Tab {
+public class CharacterTab : Tab
+{
+    //Player character 
+    private Vector2 m_playerScrollPos;
 
     public CharacterTab()
     {
@@ -19,19 +22,29 @@ public class CharacterTab : Tab {
     {
         GUILayout.Label("Player Character", EditorStyles.boldLabel);
 
-        Debug.Log("Window Size: " + m_windowSize);
-
+        //Creates the dimensions for the main box
+        Rect mainBoxRect = new Rect(20, 50, m_windowSize.width - 40, m_windowSize.height - 70);
         //Draws the main box and sets the width and height to adapt to the window changing size
-        GUILayout.BeginArea(new Rect(20, 50, m_windowSize.width - 40, m_windowSize.height - 70));
+        GUILayout.BeginArea(mainBoxRect);
 
-        //Debug buttons to check area size
+        //Creats the dimensions for the player scroll view
+        Rect playerScrollRect = new Rect(0, 0, 200, mainBoxRect.height / 2);
+        //Draws the player characters list
+        m_playerScrollPos = GUI.BeginScrollView(playerScrollRect, m_playerScrollPos, new Rect(0, 0, playerScrollRect.width - 20, 2000));
+   
+        
+        //Debug buttons to check scroll view size
         for (int i = 0; i < 100; i++)
         {
-            if (GUILayout.Button("New Player"))
+            //Creates a buttons with a interval of 20 between each button and with a width 20 less than the scroll view to allow for the scroll bar.
+            if (GUI.Button(new Rect(0, i * 20, playerScrollRect.width - 20, 20), "New Player"))
             {
 
             }
         }
+        
+
+        GUI.EndScrollView();
 
         //Ends the main box
         GUILayout.EndArea();
