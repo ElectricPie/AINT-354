4e64cd3815ee805bfa094fa27e8bd50b522d6378
Connor Priest <32139670+ElectricPie@users.noreply.github.com>
2018-11-20 14:42:26 +0000

    Removed Old Files + New Character Tab
    Removed old files as they were causing issues.
    Added new Character Tab script.

---
+++ Assets/Editor/RPGWizzard/Tabs/CharacterTab.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CharacterTab : Tab {
+
+    public CharacterTab()
+    {
+        m_tabName = "Characters";
+    }
+
+    public override void DisplayTab()
+    {
+        
+    }
+}
