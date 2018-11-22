ee1c66faf8477ca8f2a7b91a1058479b16206adb
ElectricPie <32139670+ElectricPie@users.noreply.github.com>
2018-11-22 18:24:32 +0000

    Added Character Templates Back

---
+++ Assets/Editor/RPGWizzard/Characters/CharacterTemplate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CharacterTemplate
+{
+    protected string m_name;
+    protected float m_speed;
+    protected int m_level;
+
+    public string Name
+    {
+        get { return m_name; }
+        set { m_name = value; }
+    }
+
+    public float Speed
+    {
+        get { return m_speed; }
+        set { m_speed = value; }
+    }
+
+    public int Level
+    {
+        get { return m_level; }
+        set { m_level = value; }
+    }
+}
