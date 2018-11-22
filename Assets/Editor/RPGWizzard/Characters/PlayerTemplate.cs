fc53ed451bd6d99e8ffa006b304fcdedc6294362
ElectricPie <32139670+ElectricPie@users.noreply.github.com>
2018-11-22 18:27:40 +0000

    Added Code

--- Assets/Editor/RPGWizzard/Characters/PlayerTemplate.cs
+++ Assets/Editor/RPGWizzard/Characters/PlayerTemplate.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerTemplate : MonoBehaviour {
+[System.Serializable]
+public class PlayerTemplate : CharacterTemplate
+{
+    private int m_maxLevel;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private AnimationCurve m_experienceCurve;
+
+    public AnimationCurve ExperianceCurve
+    {
+        get { return m_experienceCurve; }
+        set { m_experienceCurve = value; }
+    }
+
+    public int MaxLevel
+    {
+        get { return m_maxLevel; }
+        set { m_maxLevel = value; }
+    }
 }
