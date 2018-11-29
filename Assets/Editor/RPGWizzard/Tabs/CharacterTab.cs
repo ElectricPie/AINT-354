3a676dd12034c31fa08dc7b5d470bd65095ce12b
ElectricPie <32139670+ElectricPie@users.noreply.github.com>
2018-11-29 18:53:44 +0000

    Added General Properties
    Moved general character properties into their own method and added Name, Discription and Starting Levels.

--- Assets/Editor/RPGWizzard/Tabs/CharacterTab.cs
+++ Assets/Editor/RPGWizzard/Tabs/CharacterTab.cs
@@ -12,10 +12,17 @@ public class CharacterTab : Tab
     private Vector2 m_hostileScrollPos;
 
     //General
-    public int characterListTab = 0;
-    private int m_numberOfChar = 20;
+    private int m_characterListTab = 0;
+    private int m_newCharStartingLevel;
 
     private CharacterList m_characters;
+
+    private Rect m_mainBoxRect;
+    private Rect m_playerScrollRect;
+    private Rect m_propertiesBoxRect;
+
+    private string m_newCharName;
+    private string m_newCharDisc;
 
     public CharacterTab()
     {
@@ -26,39 +33,38 @@ public class CharacterTab : Tab
 
     public override void DisplayTab()
     {
-        //Creates the dimensions for the main box
-        Rect mainBoxRect = new Rect(20, 30, m_windowSize.width - 40, m_windowSize.height - 50);
+        //Updates the dimensions for the main box in relations to the window
+        m_mainBoxRect = new Rect(20, 30, m_windowSize.width - 40, m_windowSize.height - 50);
         //Draws the main box and sets the width and height to adapt to the window changing size
-        GUILayout.BeginArea(mainBoxRect);
+        GUILayout.BeginArea(m_mainBoxRect);
 
-        float scrollHeight = mainBoxRect.height / 2 - 30;
+        float scrollHeight = m_mainBoxRect.height / 2 - 30;
 
         //Draws the UI
         //Changes which list of characters is displayed.
-        characterListTab = GUI.Toolbar(new Rect(0, 0, 200, 20), characterListTab, new string[] { "Player", "Hostile" });
-        if (characterListTab == 0)
+        m_characterListTab = GUI.Toolbar(new Rect(0, 0, 200, 20), m_characterListTab, new string[] { "Player", "Hostile" });
+        if (m_characterListTab == 0)
         {
-            DrawCharacterList(mainBoxRect, m_characters.GetPlayersAsArray());
+            DrawCharacterList(m_characters.GetPlayersAsArray());
         }
         else
         {
-            DrawCharacterList(mainBoxRect, m_characters.GetHostilesAsArray());
+            DrawCharacterList(m_characters.GetHostilesAsArray());
         }
         //Draws the properties box
-        DrawPropertiesBox(mainBoxRect);
+        DrawPropertiesBox();
 
         //Ends the main box
         GUILayout.EndArea();
     }
 
-    private void DrawCharacterList(Rect mainBoxRect, CharacterTemplate[] characters)
+    private void DrawCharacterList(CharacterTemplate[] characters)
     {
-        //Creats the dimensions for the player scroll view
-        Rect playerScrollRect = new Rect(0, 22, 200, mainBoxRect.height - 22);
+        //Updates the dimensions for the player scroll view
+        m_playerScrollRect = new Rect(0, 22, 200, m_mainBoxRect.height - 22);
         //Draws the player characters list
-        m_playerScrollPos = GUI.BeginScrollView(playerScrollRect, m_playerScrollPos, new Rect(0, 0, playerScrollRect.width - 20, m_characters.GetPlayerCount() * 20));
+        m_playerScrollPos = GUI.BeginScrollView(m_playerScrollRect, m_playerScrollPos, new Rect(0, 0, m_playerScrollRect.width - 20, m_characters.GetPlayerCount() * 20));
 
-        if (GUI.Button(new Rect(0, 0, playerScrollRect.width - 20, 20), "New Character")) {
             
         }
 
@@ -66,7 +72,7 @@ public class CharacterTab : Tab
         for (int i = 0; i < m_characters.GetPlayerCount(); i++)
         {
             //Creates a buttons with a interval of 20 between each button and with a width 20 less than the scroll view to allow for the scroll bar.
-            if (GUI.Button(new Rect(0, i * 20 + 21, playerScrollRect.width - 20, 20), "Player [" + i + "]"))
+            if (GUI.Button(new Rect(0, i * 20 + 21, m_playerScrollRect.width - 20, 20), "Player [" + i + "]"))
             {
 
             }
@@ -75,23 +81,40 @@ public class CharacterTab : Tab
         GUI.EndScrollView();
     }
 
-    private void DrawPropertiesBox(Rect mainBoxRect)
+    private void DrawPropertiesBox()
     {
         //Creates the dimensions for the characters properties box
-        Rect propertiesBoxRect = new Rect(210, 0, mainBoxRect.width - 220, mainBoxRect.height);
+        m_propertiesBoxRect = new Rect(210, 0, m_mainBoxRect.width - 220, m_mainBoxRect.height);
         //Drawns the properties of the character 
-        GUILayout.BeginArea(propertiesBoxRect);
+        GUILayout.BeginArea(m_propertiesBoxRect);
 
-        //Creates a buttons with a interval of 20 between each button and with a width 20 less than the scroll view to allow for the scroll bar.
-        for (int i = 0; i < 100; i++)
-        {
-            if (GUI.Button(new Rect(0, i * 20, propertiesBoxRect.width, 20), "Property"))
-            {
-
-            }
-        }
+        DrawGeneralProperties();
 
         //Ends the properties box
         GUILayout.EndArea();
+    }
+
+    private void DrawGeneralProperties()
+    {
+        //Sets the width that all tags will be
+        float tagLength = 70;
+        //Sets the height that all properties will be
+        float propertyHeight = 20;
+        //Sets the distance between each propertys 
+        float propertyGap = 21;
+        //Sets the width of the fields which change with the window width
+        float fieldWidth = m_propertiesBoxRect.width - tagLength;
+
+        //Name lable and field
+        GUI.Label(new Rect(0, propertyGap * 0, tagLength, propertyHeight), "Name");
+        m_newCharName = GUI.TextField(new Rect(tagLength, propertyGap * 0, fieldWidth, propertyHeight), m_newCharName);
+
+        //Discription lable and field
+        GUI.Label(new Rect(0, propertyGap * 1, tagLength, propertyHeight), "Discription");
+        m_newCharDisc = GUI.TextArea(new Rect(tagLength, propertyGap * 1, fieldWidth, 60), m_newCharDisc);
+
+        //Starting level lable and field
+        GUI.Label(new Rect(0, propertyGap * 4, tagLength, propertyHeight), "Discription");
+        m_newCharStartingLevel = EditorGUI.IntField(new Rect(tagLength, propertyGap * 4, fieldWidth, propertyHeight), m_newCharStartingLevel);
     }
 }
