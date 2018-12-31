7623c5fe068a397a1aa43031a048f03e585501da
Connor Priest <Kon_Priest@Hotmail.com>
2018-12-31 14:28:32 +0000

    Create Scriptable Object Utilities
    Create a scriptable object utility class which will handle scriptable object.
    Currently can save a new object.

---
+++ Assets/Editor/RPGWizzard/ScriptableObjUtil.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ScriptableObjUtil {
+    public void CreateNewScriptableObj(ScriptableObject obj, string objName, string path)
+    {
+        AssetDatabase.CreateAsset(obj, path + objName + ".asset");
+        AssetDatabase.SaveAssets();
+    }
+}
