4cc4505821ecba96a34115f4178f2367eb946dfd
Connor Priest <Kon_Priest@Hotmail.com>
2019-01-20 21:46:16 +0000

    Removed Name Vairable
    Removed Name variable to prevent issues with attribute name and object name.

--- Assets/Editor/RPGWizzard/Attributes/ScriptableAttribute.cs
+++ Assets/Editor/RPGWizzard/Attributes/ScriptableAttribute.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Attribute", menuName = "RPGWizzard/Attributes/New Attributes")]
 public class ScriptableAttribute : ScriptableObject
 {
-    public new string name;
+    //public new string name;
     public string sName;
     public string disc;
     public int baseValue;
