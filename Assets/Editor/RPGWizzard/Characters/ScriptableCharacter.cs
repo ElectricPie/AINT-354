0a61b2304a6558c39422fe271aca8f467705f63b
Connor Priest <Kon_Priest@Hotmail.com>
2019-01-23 18:47:16 +0000

    Removed New Name
    Removed the new name variable to prevent issues.

--- Assets/Editor/RPGWizzard/Characters/ScriptableCharacter.cs
+++ Assets/Editor/RPGWizzard/Characters/ScriptableCharacter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScriptableCharacter : ScriptableObject {
-    public new string name;
     public string discription;
 
     public int level;
