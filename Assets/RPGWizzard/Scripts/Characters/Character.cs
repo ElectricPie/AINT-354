fd71f33e623571b0e009da1113469ca8d6090778
Connor Priest <Kon_Priest@Hotmail.com>
2019-01-24 05:45:33 +0000

    Added Missing Vairables

--- Assets/RPGWizzard/Scripts/Characters/Character.cs
+++ Assets/RPGWizzard/Scripts/Characters/Character.cs
@@ -6,35 +6,12 @@ using UnityEngine;
 public class Character : MonoBehaviour {
     [SerializeField]
     private string m_name;
-    private int m_health;
-    private int m_resources;
     [SerializeField]
     private float m_speed = 8;
+    private List<Attribute> m_attributes;
     private int m_level;
 
     private bool m_alive;
-
-
-    public void TakeDamage(int damage)
-    {
-        //Removes the damage from the characters health
-        m_health -= damage;
-
-        UpdateIfAlive();
-    }
-
-    private void UpdateIfAlive()
-    {
-        //Checks if the characters health is below the death threshold
-        if (m_health <= 0)
-        {
-            m_alive = false;
-        }
-        else
-        {
-            m_alive = true;
-        }
-    }
 
     public string Name
     {
@@ -42,20 +19,16 @@ public class Character : MonoBehaviour {
         set { m_name = value; }
     }
 
-    public int Heath
-    {
-        get { return m_health; }
-    }
-
-    public int Resources
-    {
-        get { return m_resources; }
-    }
-
     public float Speed
     {
         get { return m_speed; }
         set { m_speed = value; }
+    }
+
+    public List<Attribute> Attributes
+    {
+        get { return m_attributes; }
+        set { m_attributes = value; }
     }
 
     public int Level
